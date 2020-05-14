0e7096c0a776db5b56d27b374c682d9de5df5c7c
heasun0111 <48648940+heasun0111@users.noreply.github.com>
2020-05-14 16:13:09 +0900

    Update bank_account.py
    Menu 클래스 생성

--- bank_account.py
+++ bank_account.py
@@ -1,38 +1,38 @@
 import account_function
 
 f = account_function.Account_function()
+class Menu():
+    def Bank_menu():
+        while (1):
+            print('''
+            ======Bank Menu======
+            1. 계좌개설
+            2. 입금하기
+            3. 출금하기
+            4. 전체조회
+            5. 프로그램 종료
+            =====================
+            ''')
+            choice = int(input('입력:'))
 
-def Bank_menu():
-    while (1):
-        print('''
-        ======Bank Menu======
-        1. 계좌개설
-        2. 입금하기
-        3. 출금하기
-        4. 전체조회
-        5. 프로그램 종료
-        =====================
-        ''')
-        choice = int(input('입력:'))
+            if choice == 1:
+                f.Making_account()
 
-        if choice == 1:
-            f.Making_account()
+            elif choice == 2:
+                f.Deposit_money()
 
-        elif choice == 2:
-            f.Deposit_money()
+            elif choice == 3:
+                f.Withdraw_money()
 
-        elif choice == 3:
-            f.Withdraw_money()
+            elif choice == 4:
+                f.Checking_account()
 
-        elif choice == 4:
-            f.Checking_account()
+            elif choice == 5:
+                print("##프로그램을 종료합니다##")
+                break
 
-        elif choice == 5:
-            print("##프로그램을 종료합니다##")
-            break
-
-        else:
-            print("잘못된 값을 입력하였습니다.")
+            else:
+                print("잘못된 값을 입력하였습니다.")
 
 if __name__=='__main__':
-    Bank_menu()
+    Menu.Bank_menu()
